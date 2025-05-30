838157c518dea8cb2d50d0e39377ba9b419cc087
naymiazaki <nayana_mm@hotmail.com>
2025-05-29 21:16:18 -0300

    Preenchimento dos valores ausentes

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ import pandas as pd
 import missingno as mscno
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.dummy import DummyClassifier
 
 #Importar arquivo csv
 path = r"synthetic_dog_breed_health_data.csv"
@@ -73,14 +81,27 @@ for notnum_col in object_columns:
 # plt.show()
 
 #Limpeza dos dados:
-##Eliminar as colunas com muitos elementos ausentes
-df_clean = df.drop(columns=['Breed','Spay/Neuter Status','Daily Activity Level','Owner Activity Level'])
+##Eliminar as colunas com muitos elementos ausentes e colunas desnecessárias
+print('Nome das colunas:')
+print(df.columns.tolist())
+print("-" * 30)
+
+df_clean = df.drop(columns=['Spay/Neuter Status','Daily Walk Distance (miles)','Synthetic','Seizures',
+                            'Play Time (hrs)','Annual Vet Visits','Average Temperature (F)'])
 print(df_clean)
 print("-" * 30)
 
 ##Quantidade de colunas restantes com elementos ausentes
 print(df_clean.isna().any(axis=1).sum())
 print("-" * 30)
+
+##Atualizar as colunas com o novo data frame
+float_columns = df_clean.select_dtypes(include=np.number)
+print(f'Colunas numéricas: {float_columns}')
+print('-'*30)
+object_columns = df_clean.select_dtypes(exclude=np.number)
+print(f'Colunas não numéricas: {object_columns}')
+print('-'*30)
 
 ##Preencher valores ausentes com a média nas colunas numéricas
 for num_col in float_columns:
@@ -89,15 +110,63 @@ for num_col in float_columns:
 print(df_clean[num_col])
 print('-'*30)
 
+##Preencher valores ausentes com a moda nas colunas não numéricas
+for obj_col in object_columns:
+    columns_mode = df_clean[obj_col].mode()[0]
+    df_clean[obj_col] = df_clean[obj_col].fillna(columns_mode)
+print(df_clean[obj_col])
+print('-' * 30)
+
 print('Dados nulos em cada coluna:')
 print(df_clean.isnull().sum())
 print('-'*30)
 
-##Preencher valores ausentes com a moda nas colunas não numéricas
-# for obj_col in object_columns:
-#     columns_mode = df_clean[obj_col].mode()
-#     df_clean[obj_col] = df_clean[obj_col].fillna(columns_mode)
-# print(df_clean[obj_col])
-# print('-' * 30)
+#Modelos preditivos
+##Definição do x (features) e Y
+x = df_clean.drop('Healthy', axis=1)
+y = df_clean['Healthy']
+
+##Separar colunas numéricas e não numéricas do X
+x_num = x.select_dtypes(include=np.number).columns.tolist()
+x_obj = x.select_dtypes(exclude=np.number).columns.tolist()
+x_obj_encoded = x[x_obj]
+
+##Trasnformar dados não numéricos em numéricos para a ML compreender
+# le = LabelEncoder()
+# oe = OneHotEncoder()
+# x_encoded = oe.fit_transform(x_obj_encoded)
+# y_encoded = le.fit_transform(y)
+#
+# print(x_encoded)
+# print(y)
+#
+# print("Tipos de dados em x_train antes do escalamento:")
+# print(x_obj_encoded.dtypes)
+# print("Colunas 'object' (strings) em x_train:")
+# print(x_obj_encoded.select_dtypes(include='object').columns.tolist())
 
 
+##Separar em treinamento e teste
+# SEED = 20
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=SEED, stratify = y)
+# scale = MinMaxScaler()
+# x_train = scale.fit_transform(x_train)
+# x_test = scale.fit_transform(x_test)
+#
+# model = DecisionTreeClassifier()
+# model.fit(x_train, y_train)
+# predict = model.predict(x_test)
+# accuracy = accuracy_score(y_test,predict) * 100
+# print(f'Acurácia da árvore de decisão: {accuracy}')
+#
+# model = SVC()
+# model.fit(x_train, y_train)
+# predict = model.predict(x_test)
+# accuracy = accuracy_score(y_test,predict) * 100
+# print(f'Acurácia do SVM: {accuracy}')
+#
+# model = DummyClassifier()
+# model.fit(x_train, y_train)
+# predict = model.predict(x_test)
+# accuracy = accuracy_score(y_test,predict) * 100
+# print(f'Acurácia do DummyClassifier: {accuracy}')
